394004bc739f7ac43ca5ed7070a71faca8acc139
FatimaGC <73245376+FatimaGC@users.noreply.github.com>
2023-02-23 01:25:51 +0000

    added Primary button code to Button.stories.jsx

--- src/stories/Button.stories.js
+++
@@ -1,6 +0,0 @@
-import Button from '../components/Button'
-
-export default {
-    title: "Button",
-    component: Button
-}
---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Button from '../components/Button'
+
+export default {
+    title: "Button",
+    component: Button,
+
+    argTypes: {
+        backgroundColor: {control: 'color'},
+    },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {  
+    primary: true,  
+    label: 'Button',
+};
+
