69fc41b010e84ce2d669d890a13ac48051acc483
FatimaGC <73245376+FatimaGC@users.noreply.github.com>
2023-06-09 20:49:25 +0000

    Create scaffolding for Avatar component

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"; 
+import './styles.css';
+
+function Avatar(){
+
+}
+
+export default Avatar;
---
+++ src/stories/Avatar.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Avatar from '../components/Avatar'
+
+export default {
+    title: "Avatar",
+    component: Avatar,
+
+    argTypes: {
+        backgroundColor: {control: 'color'},
+    },
+};
+
+
