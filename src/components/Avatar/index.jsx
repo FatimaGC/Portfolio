6f7c69954333dd98a781b383faaf84e56a3cc7f8
FatimaGC <73245376+FatimaGC@users.noreply.github.com>
2023-08-05 22:14:11 +0000

    Add getInitials function

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -3,6 +3,16 @@ import './styles.css';
 
 function Avatar({src, variant="PRIMARY", firstName, lastName}){
     const username = `${firstName || " "} ${lastName || " "}`.trim(); 
+    const getInitials = (firstName, lastName) => {
+        const firstInitial = firstName?.slice?.(0,1);
+        const lastInitial = lastName?.slice?.(0,1);
+
+        if (firstInitial && lastInitial) {
+            return `${firstInitial}${$lastInitial}`
+        }
+    return null;
+    }
+    const initials = getInitials(firstName, lastName);
     const shouldShowImage = Boolean(src || (!src && !username));
     return (
         <figure>
