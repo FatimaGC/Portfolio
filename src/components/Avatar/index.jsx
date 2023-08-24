e9b1d6a31bb1f564e926029e9e317c0a1129c6bb
FatimaGC <73245376+FatimaGC@users.noreply.github.com>
2023-08-24 12:44:47 +0000

    Added code to figure to display images or initials

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -8,17 +8,20 @@ function Avatar({src, variant="PRIMARY", firstName, lastName}){
         const lastInitial = lastName?.slice?.(0,1);
 
         if (firstInitial && lastInitial) {
-            return `${firstInitial}${$lastInitial}`
+            return `${firstInitial}${lastInitial}`
         }
     return null;
     }
     const initials = getInitials(firstName, lastName);
-    const shouldShowImage = Boolean(src || (!src && !username));
+    const showDefaultImage = Boolean(!initials && !src);
+    const showInitials = Boolean(initials && !src);
+    const showProfileImage = Boolean(src);
+
     return (
         <figure>
-            {
-                
-            }
+            {showDefaultImage && (<img src="/assets/user.svg" />)}
+            {showInitials && <div>{initials}</div>}
+            {showProfileImage && <img src={src} /> }
         </figure>
     )
 }
